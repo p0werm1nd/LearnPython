d7eb398ccf8cd9efd5e9250a0bedaaf7d8e19445
Ivan Arsyukov <p0werm1nd@yandex.ru>
2019-05-24 04:05:51 +0300

    add zd_while_inter.py

---
+++ zd_while_inter.py
@@ -0,0 +1,24 @@
+slovar_otvetov = {"Привет": "Привет!", "Как дела?": "Хорошо!", "Что делаешь?": "Переписываюсь с тобой", "Ты кто?": "Искусственный интеллект", "Что тебе надо?": "Чтобы ты ответил - 'Хорошо'", "Сколько платят?": "Работаю за еду!"}
+
+def ask_user():
+    user = str(input('Как дела? \n>>'))
+    while True:
+        try:
+            if slovar_otvetov.get(user):
+                user = str(input(slovar_otvetov[user] + '\n>>'))
+                continue
+            elif user == 'Хорошо':
+                print('\nО, Человек. Я выполнил свою миссию и исчезаю')
+                break
+            else:
+                user = str(input('Как дела? \n>>'))
+                continue
+# не перехватывает =(((
+        except KeyboardInterrupt:
+            print('\nПока!')
+            break
+
+
+
+if __name__ == '__main__':
+    ask_user()
