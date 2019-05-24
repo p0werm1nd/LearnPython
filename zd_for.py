ba641586ecfa8595b4cddb7983f71cf6ab01021e
Ivan Arsyukov <i-arsyukov@yandex-team.ru>
2019-05-24 12:04:49 +0300

    add text

--- zd_for.py
+++ zd_for.py
@@ -1,3 +1,10 @@
+#Домашнее задание №1
+#Цикл for: Оценки
+#* Создать список из словарей с оценками учеников разных классов
+#  школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
+#* Посчитать и вывести средний балл по всей школе.
+#* Посчитать и вывести средний балл по каждому классу.
+
 def main():
 
     classy_ocenki = [{'class': '1a', 'ocenki': [3, 4, 4, 3]},
@@ -18,6 +25,6 @@ def main():
 #        print (kagdyj_class)
         print('Средний балл в классе ', kagdyj_class['class'], ' = ', sum(kagdyj_class['ocenki']) / len(kagdyj_class['ocenki']))
     return
-   
+
 if __name__ == "__main__":
     main()
